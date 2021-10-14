a5cb740741f4195444c03679cc48ecec64787273
Arlo O'Keeffe <tiger@k5d.de>
2021-10-14 21:47:55 +0200

    Remove unused stuff

--- docker-sample/project-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-set -o xtrace
-
-docker compose up --detach
-
-mutagen project start
--- docker-sample/project-stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-set -o xtrace
-
-mutagen project terminate || true
